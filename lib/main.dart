b9e4e385cfba44d3ec1c598b65446f92278097d5
Kuldeep Rathor <90392158+KuldeepRathor@users.noreply.github.com>
2024-01-22 23:30:21 +0530

    firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:mind_map/features/auth/google_sign_in/sign_in_screen.dart';
 import 'package:mind_map/widgets/btnavbar.dart';
 
-void main() => runApp(const MyApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
